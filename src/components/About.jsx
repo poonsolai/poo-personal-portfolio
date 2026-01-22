a7a028b618dd06a9772b821d612ab0c34b3afae6
poonsolai <poonsolai05@gmail.com>
2026-01-22 05:59:19 +0530

    update files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import {   faEnvira, faRProject,faStackOverflow,faXbox } from '@fortawesome/free-brands-svg-icons'
+import {   faBrain, faCompass, faCube, faCubes, faIcons, faLaptopCode, faLayerGroup, faUserGraduate } from '@fortawesome/free-solid-svg-icons'
 import './About.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
@@ -7,22 +8,22 @@ function Aboutme1(){
     return (
         <div className='col-md-4 ' >
             <div className='about-box' style={{borderLeft:"5px solid blue"}}>
-                <FontAwesomeIcon icon={faXbox} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faUserGraduate} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
                 <h1>2027</h1>
                 <p>Graduate</p>
             </div>
             <div className='about-box' style={{borderLeft:"5px solid red"}}>
-                <FontAwesomeIcon icon={faRProject} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faLaptopCode} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
                 <h1>10+</h1>
                 <p>Projects</p>
             </div>
             <div className='about-box' style={{borderLeft:"5px solid pink"}}>
-                <FontAwesomeIcon icon={faStackOverflow} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faLayerGroup} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
                 <h1>6</h1>
                 <p>Tech Stack</p>
             </div>
             <div className='about-box-final'>
-                <h4> <FontAwesomeIcon icon={faEnvira}></FontAwesomeIcon> Interests</h4>
+                <h4> <FontAwesomeIcon icon={faIcons}></FontAwesomeIcon> Interests</h4>
                 <ul>
                     <li>Web Development</li>
                     <li>Mobile Apps</li>
@@ -43,21 +44,21 @@ function Aboutme2(){
                 <p>I am a recent B.Tech Information Technology graduate with a strong interest in building modern, user-friendly web applications. My journey in technology started during college, where I discovered my passion for transforming ideas into functional and visually appealing digital solutions through code.</p>
             </div>
             <div className='about-box-final pb-1' style={{borderLeft:"5px solid red"}}>
-                <FontAwesomeIcon icon={faRProject} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faUserGraduate} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
                 <h3>Education</h3>
                 <ul>
-                    <li>Bachelor of Technology - Information Technology (B.Tech IT)</li>
-                    <li>Shreenivasa Engineering College</li>
+                    <li>Bachelor of Technology - Information Technology (B.Tech IT),</li>
+                    <li>Shreenivasa Engineering College,</li>
                     <li>Graduated: 2027 | CGPA: 8.5 / 10</li>
                 </ul>
             </div>
             <div className='about-box-final pb-1' style={{borderLeft:"5px solid pink"}}>
-                <FontAwesomeIcon icon={faStackOverflow} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faCompass} style={{width:"30px",height:"30px"}}></FontAwesomeIcon>
                 <h3>What I'm Looking For</h3>
                 <p>I am actively seeking opportunities as a Junior Frontend Developer / Web Developer, where I can apply my technical skills, learn from experienced professionals, and grow in a collaborative environment. I am particularly interested in roles that involve React, modern JavaScript, responsive design, and Ul-focused development.</p>
             </div>
             <div className='about-box-final pb-1'>
-                <h4> <FontAwesomeIcon icon={faEnvira}></FontAwesomeIcon> My Approach</h4>
+                <h4> <FontAwesomeIcon icon={faBrain}></FontAwesomeIcon> My Approach</h4>
                 <ul>
                     <li>Fast learner with strong problem-solving abilities</li>
                     <li>Detail-oriented and focused on writing clean, maintainable code</li>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,66 +1,146 @@
 import './Contact.css'
-import {  faHillAvalanche, faLocationDot, faMailBulk, faPhoneAlt} from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faLinkedinIn, faSquareUpwork, faWizardsOfTheCoast} from "@fortawesome/free-brands-svg-icons"
+import { faLocationDot, faMailBulk, faPhoneAlt} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faLinkedinIn, faWhatsapp, faWizardsOfTheCoast} from "@fortawesome/free-brands-svg-icons"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { use, useState } from 'react'
 
 const Contactme = () => {
   return (
     <div className='col-md-4'>
-            <div className='mail-box'>
-                <FontAwesomeIcon icon={faMailBulk} className='icon'></FontAwesomeIcon>
-                <div>
-                    <h5>E-Mail</h5>
-                    <p>poonsolai05@gmail.com</p>
-                </div>
-            </div>
-            <div className='mail-box'>
-                <FontAwesomeIcon icon={faPhoneAlt} className='icon'></FontAwesomeIcon>
-                <div>
-                    <h5>Phone</h5>
-                    <p>+91 9043415306</p>
-                </div>
-            </div>
+           
+                <a href="mailto:poonsolai05@gmail.com" className='mail-box'>
+                    <FontAwesomeIcon icon={faMailBulk} className='icon'></FontAwesomeIcon>
+                    <div>
+                        <h5>E-Mail</h5>
+                        <p>poonsolai05@gmail.com</p>
+                    </div>
+                </a>
+
+                <a href="https://wa.me/919043415306" className='mail-box' target='_blank'>
+                    <FontAwesomeIcon icon={faPhoneAlt} className='icon'></FontAwesomeIcon>
+                    <div>
+                        <h5>Phone</h5>
+                        <p>+91 9043415306</p>   
+                    </div>
+                </a>
         
-            <div className='mail-box '>
-                <FontAwesomeIcon icon={faLocationDot} className='icon'></FontAwesomeIcon>
-                <div>
-                    <h5>Location</h5>
-                    <p>Mittanoolahalli ,Dharmapuri (dt) ,TamilNadu</p>
+                <a href="https://www.google.com/maps/place//@12.0767112,78.201733,53m/data=!3m1!1e3!4m9!1m8!3m7!1s0x3bac1169b81204b1:0x994a544114fd5e41!2sNoolahalli,+Tamil+Nadu!3b1!8m2!3d12.0717919!4d78.1838273!16s%2Fg%2F12hpfns62?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoASAFQAw%3D%3D" className='mail-box ' target='_blank'>
+                    <FontAwesomeIcon icon={faLocationDot} className='icon'></FontAwesomeIcon>
+                    <div>
+                        <h5>Location</h5>
+                        <p className='para'>Mittanoolahalli ,Dharmapuri (dt) ,TamilNadu</p>
+                    </div>
+                </a>
+          
+                <a href="https://github.com/poonsolai" className='mail-box mt-5' target='_blank'>
+                    <FontAwesomeIcon icon={faGithub} className='icon'></FontAwesomeIcon>
+                    <div>
+                        <h5>GitHub</h5>
+                        <p>@poonsolai</p>
+                    </div>
+                </a>
+
+                <a href="https://www.linkedin.com/in/poonsolai-karthick-58683117" className='mail-box'  target='_blank'>
+                    <FontAwesomeIcon icon={faLinkedinIn} className='icon'></FontAwesomeIcon>
+                    <div>
+                        <h5>LinkedIn</h5>
+                        <p >@poonsolai</p>
+                    </div>
+                </a>
+           
+                <div className="work-box">
+                    <FontAwesomeIcon icon={faWizardsOfTheCoast} className='icon'></FontAwesomeIcon>
+                    <div>
+                        <h3 className=' text-center'>Available for Work</h3>
+                        <p className=' text-center'>Open to full-time positions and internship opportunities</p>
+                    </div>
                 </div>
-            </div>
-        
-            <div className='mail-box mt-5'>
-                <FontAwesomeIcon icon={faGithub} className='icon'></FontAwesomeIcon>
-                <div>
-                    <h5>GitHub</h5>
-                    <a href="" style={{textDecoration:"none",color:"white"}}>@poonsolai</a>
-                </div>
-            </div>
-        
-            <div className='mail-box'>
-                <FontAwesomeIcon icon={faLinkedinIn} className='icon'></FontAwesomeIcon>
-                <div>
-                    <h5>LinkedIn</h5>
-                    <a href="" style={{textDecoration:"none",color:"white"}}>@poonsolai</a>
-                </div>
-            </div>
-        
-            <div className='work-box'>
-                <FontAwesomeIcon icon={faWizardsOfTheCoast} className='icon'></FontAwesomeIcon>
-                <div>
-                    <h3 className=' text-center'>Available for Work</h3>
-                    <p className=' text-center'>Open to full-time positions and internship opportunities</p>
-                </div>
-            </div>
         
     </div>
   )
 }
 
 const Contactform = () => {
+    // show the message box ana variable 
+    const [box,setBox] = useState(false);
+    // form state variable .. store the value this variable 
+    const [msg,setMsg] = useState({name:"",mail:"",subject:"",message:""});
+    
+    // form handle function
+    function Formhandle(e){
+        setMsg({...msg,[e.target.name]:e.target.value})
+    }
+    console.log(msg);
+    
+    // The input send in our whatsapp
+    function Sendmsg(e){
+        e.preventDefault();
+        let message = `Hello poonsolai ,My name :${msg.name} ,`
+        +`Mail id : ${msg.mail} ,
+        Subject : ${msg.subject} ,
+        Message : ${msg.message}`;
+        window.open(`https://wa.me/919043415306?text=${encodeURIComponent(message)}`,'_blank');
+        setMsg({name:"",mail:"",subject:"",message:""});
+    }
+    // The input is send mail
+    function Sendmail(e){
+        e.preventDefault();
+        window.open(`mailto:poonsolai05@gmail.com?subject=${encodeURIComponent(msg.subject)}&body=${encodeURIComponent(msg.message)}`,'_blank');
+        setMsg({name:"",mail:"",subject:"",message:""});
+    }
+
+
   return (
-    <div className='col-md-8'>
-      
+    <div className='col-md-8 form-box mt-3 rounded-2' >
+        <div className="btns">
+           {
+            box &&  <button onClick={()=>{setBox(false);setMsg({name:"",mail:"",subject:"",message:""});}}><FontAwesomeIcon icon={faWhatsapp} /> Whatsapp</button> 
+           }
+           {
+            !box && <button onClick={()=>{setBox(true);setMsg({name:"",mail:"",subject:"",message:""});}}><FontAwesomeIcon icon={faMailBulk} /> Mail</button>
+           }
+        </div>
+        {   !box && 
+            <form>
+                <h1>Send Me a Message</h1>
+                <div className="name-box">
+                    <label htmlFor="name">Your Name:</label>
+                    <input type="text" id='name' placeholder='poonsolaikarthick ' name='name' onChange={Formhandle} value={msg.name}/>
+                </div>
+                <div className="mail1-box">
+                    <label htmlFor="mail">Your Email:</label>
+                    <input type="mail" id='mail' placeholder='poonsolai05gmailcom' name='mail' onChange={Formhandle} value={msg.mail}/>
+                </div>
+                <div className="sub-box">
+                    <label htmlFor="sub">Subject:</label>
+                    <input type="text" id='sub' placeholder='Job Opportunity / Collaboration' name='subject' onChange={Formhandle} value={msg.subject}/>
+                </div>
+                <div className="area">
+                    <label htmlFor="area">Message</label>
+                    <textarea name="message" id="area"  placeholder='Tell me about your project or opportunity ...' onChange={Formhandle}  rows={10} value={msg.message}>
+
+                    </textarea>
+                </div>
+                <button  className='btn w-100 bg-info mt-2'onClick={Sendmsg}>Send Message</button>
+        </form>
+        }
+         {
+            box && 
+            <form >
+                <h1>Send Me a Mail</h1>
+                <div className="sub-box">
+                    <label htmlFor="sub">Subject :</label>
+                    <input type="text" id='sub' placeholder='Job Opportunity / Collaboration' name='subject' onChange={Formhandle} value={msg.subject}/>
+                </div>
+                <div className="area">
+                    <label htmlFor="area">Message :</label>
+                    <textarea name="message" id="area"  placeholder='Tell me about your project or opportunity ...' onChange={Formhandle} rows={10} value={msg.message}>
+
+                    </textarea>
+                </div>
+                <button  className='btn w-100 bg-info mt-2'onClick={Sendmail}>Send Message</button>
+        </form>
+         }
     </div>
   )
 }
